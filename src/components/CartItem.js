938f6ade1901d8f4621282e22332f9cb0cf06bf3
takuya_kajiwara <fd.elt.119@gmail.com>
2023-11-18 16:35:28 +0900

    💄 cart-container実装

---
+++ src/components/CartItem.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { MinusIcon, PlusIcon } from "../HeroIcons";
+
+const CartItem = (props) => {
+  const { id, img, title, price, amount } = props;
+  return (
+    <article className="cart-item" key={id}>
+      <img src={img} alt="" />
+      <div>
+        <h4>{title}</h4>
+        <h4 className="item-price">{price}円</h4>
+        <button className="remove-btn">削除</button>
+      </div>
+      <div>
+        <button className="amount-btn">
+          <PlusIcon />
+        </button>
+        <p className="amount">{amount}</p>
+        <button className="amount-btn">
+          <MinusIcon />
+        </button>
+      </div>
+    </article>
+  );
+};
+
+export default CartItem;
