3864a56c017e3232cc48e1785e51cdd1171f4f69
takuya_kajiwara <fd.elt.119@gmail.com>
2023-11-18 21:15:18 +0900

    ✨ 全削除機能実装

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,8 +1,11 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import CartItem from "./CartItem";
+import { clearCart } from "../features/cart/CartSlice";
 
 const CartContainer = () => {
+  const dispatch = useDispatch();
+
   const { amount, cartItems, total } = useSelector((state) => state.cart);
 
   if (amount < 1) {
@@ -33,7 +36,9 @@ const CartContainer = () => {
             合計 <span>{total}円</span>
           </h4>
         </div>
-        <button className="btn clear-btn">全削除</button>
+        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+          全削除
+        </button>
       </footer>
     </section>
   );
